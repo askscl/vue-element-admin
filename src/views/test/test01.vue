b96bc8ea5f1c6a76bf4d38a99299772c674ebf81
scl <419863966@qq.com>
2022-04-14 15:53:02 +0800

    格式优化2

--- src/views/test/test01.vue
+++ src/views/test/test01.vue
@@ -1,30 +1,30 @@
 <template>
-  <div>
-    <div class="tips">测试iframe通信111</div>
-    <div class="tips">{{ message }}</div>
-    <div v-show="message" class="tips">好个鬼</div>
-  </div>
+    <div>
+        <div class="tips">测试iframe通信111</div>
+        <div class="tips">{{ message }}</div>
+        <div v-show="message" class="tips">好个鬼</div>
+    </div>
 </template>
 <script>
 export default {
-  name: 'Test01',
-  data() {
-    return {
-      message: ''
-    }
-  },
-  mounted() {
-    window.addEventListener('message', (e) => {
-      const res = e.data
-      if (res.code == 1) {
-        this.message = e.data.content
-        console.log('我收到了：', e.data.content)
-        console.log('我回答：很好！')
-      }
-    })
-  },
-  methods: {}
-}
+    name: "Test01",
+    data() {
+        return {
+            message: "",
+        };
+    },
+    mounted() {
+        window.addEventListener("message", (e) => {
+            const res = e.data;
+            if (res.code == 1) {
+                this.message = e.data.content;
+                console.log("我收到了：", e.data.content);
+                console.log("我回答：很好！");
+            }
+        });
+    },
+    methods: {},
+};
 </script>
 <style lang="scss" scoped>
 </style>
--- src/views/test/test02.vue
+++ src/views/test/test02.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <div class="tips">我是测试222</div>
-  </div>
+    <div>
+        <div class="tips">我是测试222</div>
+    </div>
 </template>
 <script>
 export default {
-  name: 'Test02',
-  data() {
-    return {}
-  },
-  methods: {}
-}
+    name: "Test02",
+    data() {
+        return {};
+    },
+    methods: {},
+};
 </script>
 <style lang="scss" scoped>
 </style>
