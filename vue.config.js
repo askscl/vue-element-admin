057830023e3d43405729bc87d5d9e8bc4a64bfc0
scl <419863966@qq.com>
2022-06-13 17:33:48 +0800

    开启调试

--- vue.config.js
+++ vue.config.js
@@ -64,6 +64,7 @@ module.exports = {
         // provide the app's title in webpack's name field, so that
         // it can be accessed in index.html to inject the correct title.
         name: name,
+        devtool: process.env.NODE_ENV === 'development' ? 'source-map' : false,
         resolve: {
             alias: {
                 '@': resolve('src')
