3c23152c34e16ee04c58479a71204216a40870f9
homeScl <419863966@qq.com>
2023-06-06 16:10:38 +0800

    JS练习

---
+++ src/views/jsLearn/ajax.js
@@ -0,0 +1,10 @@
+const xhr = new XMLHttpRequest();
+xhr.open('get', 'aabb.php', true);
+xhr.send(null);
+xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4){
+        if(xhr.status == 200){
+            console.log(xhr.responseText);
+        }
+    }
+}
---
+++ src/views/jsLearn/ajaxPromise.js
@@ -0,0 +1,22 @@
+const getJson = function(url, type, data){
+    const promise = new Promise(function(resolve, reject){
+        const xmlHttp = new XMLHttpRequest();
+        xmlHttp.open(type, url);
+        if(type == 'get'){
+            xmlHttp.send();
+        }else{
+            xmlHttp.setRequestHeader("Content-Type", "application/json");
+            xmlHttp.send(JSON.stringify(data));
+        }
+        xmlHttp.responseType = "json";
+        xmlHttp.onreadystatechange = function(){
+            if(xmlHttp.readyState !== 4) return;
+            if(xmlHttp.status === 200){
+                resolve(xmlHttp.response);
+            }else{
+                reject(new Error(xmlHttp.statusText));
+            }
+        }
+    });
+    return promise
+}
