a687c0de901cbcf31b971b96906d680b7d890818
homeScl <419863966@qq.com>
2023-06-07 10:59:47 +0800

    1

--- src/views/jsLearn/ajaxPromise.js
+++ src/views/jsLearn/ajaxPromise.js
@@ -17,6 +17,10 @@ const getJson = function(url, type, data){
                  * Content-Type:为application/json时， 可以用response
                  * Content-Type:为text/html时，两者都可以，但是response获取到解析好的dom
                  * Content-Type:为媒体（视频，音频），二进制流时，可用response,需要设置responseType为blob, 在response上获取到Blob对象
+                 * 
+                 * this.response 获取到的结果会根据 this.responseType 的变化而变
+                 * this.responseText 永远获取的是字符串形式的响应体
+                 * 注意：在客户端这样设置responseType会有兼容问题，所以一般不这样写
                  * */
                 resolve(xmlHttp.response);
                 // resolve(xmlHttp.responseText);
