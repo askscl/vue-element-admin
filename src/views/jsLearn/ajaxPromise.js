1db41de6b62b162569218b6089f8227da09d8039
homeScl <419863966@qq.com>
2023-06-07 10:56:27 +0800

    response和responseText的区别

--- src/views/jsLearn/ajaxPromise.js
+++ src/views/jsLearn/ajaxPromise.js
@@ -12,7 +12,14 @@ const getJson = function(url, type, data){
         xmlHttp.onreadystatechange = function(){
             if(xmlHttp.readyState !== 4) return;
             if(xmlHttp.status === 200){
+                //到底用哪一个，还是两个都可以？--
+                /**
+                 * Content-Type:为application/json时， 可以用response
+                 * Content-Type:为text/html时，两者都可以，但是response获取到解析好的dom
+                 * Content-Type:为媒体（视频，音频），二进制流时，可用response,需要设置responseType为blob, 在response上获取到Blob对象
+                 * */
                 resolve(xmlHttp.response);
+                // resolve(xmlHttp.responseText);
             }else{
                 reject(new Error(xmlHttp.statusText));
             }
