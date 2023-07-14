f0b5379ad8483fd910701662133a712dbaaa9f3d
homeScl <419863966@qq.com>
2023-07-14 18:14:45 +0800

    算法学习

---
+++ src/views/jsLearn/两数之和.js
@@ -0,0 +1,41 @@
+/**
+ * 两数之和(leetcode1)
+ * 
+测试用例
+输入: nums =[2,7,11,15], target =9
+输出:[0,1]
+解释:因为 nums[0] +nums[1] ==9，返回[0,1]。
+使用时间复杂度为 0(n) 的算法。
+
+解题思路
+1.通过Map对象存储遍历过的元素和对应的索引
+2.每遍历一个元素，看看Map对象中是否存在满足要求的目标数字
+3.一次循环即能够找到符合条件的索引组
+
+
+思考：
+1.为什么要采用Map
+因为Map,可以存储键值对，可以方便获取值的键，在该例子中键是数组的下标索引
+2.出题目的
+一道题考了Map的三种使用方法：has(), get(), set()
+ */
+
+const nums =[2,7,11,15], target =9;
+
+function twoSum(arr, target){
+    let map = new Map();
+    let res = [];
+    arr.forEach((item, index) =>{
+        if(map.has(target - item)){
+            res = [index, map.get(target - item)];
+        }else{
+            map.set(item, index)
+        }
+    });
+    console.log(map)
+    console.log(res)
+    return res;
+}
+
+
+twoSum(nums, target);
---
+++ src/views/jsLearn/搜索插入位置_二分.js
@@ -0,0 +1,35 @@
+/**
+ * 算法：搜索插入位置（二分）
+ * 
+ * 测试用例
+ * 输入: arr =[1,3,5,6],target = 5
+ * 输出:2
+ * 必须使用时间复杂度为 o(log n) （以2为底）的算法
+ * 
+ * 解题思路
+ * 先设定左侧下标left和右侧下标right,再计算中间下标mid
+ * 每次根据arr[mid]和target之间的大小进行判断，相等则直接返回，arr[mid]<target则left右移，arr[mid]>target则right左移
+ * 查找结束如果没有相等值则返回left，该值为插入位置
+ */
+
+const arr = [1, 3, 5, 6], target = 5;
+let searchInsert = function (arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (right >= left) {
+        const mid = Math.floor((left + right) / 2);
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else if (arr[mid] > target) {
+            right = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+
+    return left;
+
+}
+
+const answer = searchInsert(arr, target);
+console.log(answer);
