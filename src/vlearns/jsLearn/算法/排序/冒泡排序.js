15e3fc6b77e4799d53b823d7bd779f84eadda1f3
homeScl <419863966@qq.com>
2023-08-28 18:05:20 +0800

    冒泡排序

--- src/vlearns/jsLearn/算法/排序/冒泡排序.js
+++ src/vlearns/jsLearn/算法/排序/冒泡排序.js
@@ -1,5 +1,10 @@
 const arr = [7, 4, 2, 1, 3, 5, 6];
 
+// 方法一
+/* 
+思路：
+1.每次跟后面的比较，遇到大的就交换（最后一个没有数比较了），因此外层要 -1
+*/
 function sortArr(arr){
     for(let i = 0; i < arr.length -1; i++){
         for(let j = i+1; j < arr.length; j++){
@@ -11,4 +16,25 @@ function sortArr(arr){
     return arr
 }
 
-console.log(sortArr(arr));
+// console.log(sortArr(arr));
+
+//方法二
+/* 
+思路：
+1.外层每次找到一个最大值，要找几次（比如3个要找两次）
+2.内层相邻两两交换，
+    2.1已确定的最大值不需要交换，因此  -i
+    2.2因为要用到j+1, 因此 -1
+*/
+function bubbleSort(arr){
+    for(let i = 0; i< arr.length -1; i++){
+        for(let j = 0; j < arr.length - 1 - i; j++){
+            if(arr[j] > arr[j+1]){
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort(arr));
