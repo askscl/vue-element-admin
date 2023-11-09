16d602e6a3aab2300ac0f1d6ea7b9d9291d9c117
homeScl <419863966@qq.com>
2023-11-09 10:02:47 +0800

    数组转树

--- src/vlearns/jsLearn/笔试/手写题/数组转树.js
+++ src/vlearns/jsLearn/笔试/手写题/数组转树.js
@@ -45,32 +45,26 @@ const data = [
 */
 
 const data = [
-
     { id: 2, title: '部门 2', pid: 1, children: [] },
     { id: 3, title: '部门 3', pid: 1, children: [] },
     { id: 4, title: '部门 4', pid: 3, children: [] },
     { id: 1, title: '部门 1', pid: 0, children: [] },
-    { id: 5, title: '部门 5', pid: 4, children: [] },
-    { id: 6, title: '部门 6', pid: 7, children: [] }
+    { id: 5, title: '部门 5', pid: 4, children: [] }
 ];
-
-function buildTree(data) {
-    let tree = [];
-    for (let i = 0; i < data.length; i++) {
-        const item = data[i];
-        if (item.pid === 0) {
-            tree.push(item);
-        } else {
-            const parent = data.find(p => p.id === item.pid);
-            if (parent) {
-                const children = parent.children || [];
-                children.push(item);
-                parent.children = children;
+function buildTree(data){
+    const tree =  [];
+    data.forEach(item => {
+        if(item.pid === 0 ){
+            tree.push(item)
+        }else{
+            const parent = data.find(i => i.id === item.pid);
+            if(parent){//注意考虑没找到的情况
+                parent.children.push(item);
             }
+            
         }
-    }
+    });
     return tree;
 }
 
-const tree = buildTree(data);
-console.log(JSON.stringify(tree, null, 2));
+console.log(JSON.stringify(buildTree(data), null, 2));
