7f976dc0a8620cadceaa8a6481eb1a81c450bea9
homeScl <419863966@qq.com>
2023-12-14 22:12:12 +0800

    数字格式化

--- src/vlearns/jsLearn/笔试/场景题/数字格式化.js
+++ src/vlearns/jsLearn/笔试/场景题/数字格式化.js
@@ -1,3 +1,54 @@
 /* 
 数字格式化问题:1234567890-->1,234,567,890
+1 234  567  890
+0 123  456  789
+
+思路：
+从后往前遍历，每次遍历三位，双指针，一个记录索引，一个记录截取次数
+1.通过总长度/3，向上取整，得要要遍历的次数
+2.当达到最后一次时，因为有可能只有一个，所以通过计算得到最后一次剩下的个数，作为最后一次截取的索引
+3.以头插入方式收集进数组unshift
+4.以join转换数组
+
+toString toString toString toString toString
+ceil ceil ceil ceil ceil ceil
+floor floor floor floor floor
+slice slice slice slice slice(【包含】起始索引，【不包含】末索引)--产生新数组
+unshift unshift unshift unshift unshift
+join join join join join
 */
+
+function numberFormat(num){
+    const numStr = num.toString();
+    const len = numStr.length;
+    const times = Math.ceil(len/3);
+    const arr = [];
+
+    for(let i = len - 3, j = 0; j<times; i = i - 3, j++){
+        if(j <times - 1){
+            
+            arr.unshift(numStr.slice(i, i+3));
+        }else{
+            //==times
+            const endIndex = len - 3*j;
+            arr.unshift(numStr.slice(0, endIndex))
+        }
+        // console.log(arr);
+    }
+    return arr.join(',')   
+
+}
+console.log(numberFormat(234567890));
+// console.log(numberFormat(90));
+// console.log(numberFormat(7890));
+
+
+
+/* const i = num%1000;
+    console.log(i)
+    if(i != 0){
+        arr.unshift(i);
+    } */
+// const num = '1234567890';
+// num.slice(6);
+// console.log(num)
--- src/vlearns/jsLearn/练习/test.js
+++ src/vlearns/jsLearn/练习/test.js
@@ -1,77 +1,116 @@
 /* 
-将数组处理成以下 tree结构
-const data = [
-    { id: 2, title: '部门 2', pid: 1, children: [] },
-    { id: 3, title: '部门 3', pid: 1, children: [] },
-    { id: 4, title: '部门 4', pid: 3, children: [] },
-    { id: 1, title: '部门 1', pid: 0, children: [] },
-    { id: 5, title: '部门 5', pid: 4, children: [] }
-];
-[
-  {
-    "id": 1,
-    "title": "部门 1",
-    "pid": 0,
-    "children": [
-      {
-        "id": 2,
-        "title": "部门 2",
-        "pid": 1,
-        "children": []
-      },
-      {
-        "id": 3,
-        "title": "部门 3",
-        "pid": 1,
-        "children": [
-          {
-            "id": 4,
-            "title": "部门 4",
-            "pid": 3,
-            "children": [
-              {
-                "id": 5,
-                "title": "部门 5",
-                "pid": 4,
-                "children": []
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-]
+justify-content justify-content justify-content justify-content
+justify-content
+align-items align-items align-items align-items align-items
+flex flex flex flex flex
+flex-grow, flex-grow flex-grow flex-grow flex-grow
+flex-shrink shrink shrink shrink shrink
+basis basis basis basis basis
+ceil ceil ceil ceil ceil 
 */
 
-const data = [
+/* 
+    数组
+    push push push push push
+    pop pop pop pop pop
+    reverse reverse reverse reverse reverse
+    sort sort sort sort sort
+    concat concat concat concat concat
+    unshift unshift unshift unshift unshift
+    shift shift shift shift shift
+    slice slice slice slice slice
+    splice splice splice splice splice
+    indexOf indexOf indexOf indexOf indexOf
+    lastIndexOf lastIndexOf lastIndexOf lastIndexOf lastIndexOf
+    every every every every every 
+    some some some some some
+    filter filter filter filter filter
+    map map map map map
+    forEach forEach forEach forEach
+    reduce reduce reduce reduce reduce
+    join join join join join
+    isArray isArray isArray isArray isArray
+    keys keys keys keys keys
+    values values values values values
+    entries entries entries entries entries
+    fill fill fill fill fill
+    find find find find find
+    findIndex findIndex findIndex findIndex findIndex
+    Array.from Array.from Array.from Array.from Array.froms
 
-    { id: 2, title: '部门 2', pid: 1, children: [] },
-    { id: 3, title: '部门 3', pid: 1, children: [] },
-    { id: 4, title: '部门 4', pid: 3, children: [] },
-    { id: 1, title: '部门 1', pid: 0, children: [] },
-    { id: 5, title: '部门 5', pid: 4, children: [] },
-    { id: 6, title: '部门 6', pid: 7, children: [] }
-];
+    concat concat concat concat concat
+    slice slice slice slice slice
+    filter filter filter filter filter
+    map map map map map
+    push push push push push
+    pop pop pop pop pop
+    unshift unshift unshift unshift unshift
+    shift shift shift shift shift
+    reverse reverse reverse reverse reverse
+    sort sort sort sort sort
+    concat concat concat concat concat
+    slice slice slice slice slice
+    splice splice splice splice splice
+    indexOf indexOf indexOf indexOf indexOf
+    lastIndexOf lastIndexOf lastInexOf lastIndexOf lastIndexOf
+    every every every every every
+    some some some some some
+    filter filter filter filter filter
+    map map map map map
+    forEach forEach forEach forEach
+    reduce reduce reduce reduce reduce
+    join join join join join
+    isArray isArray isArray isArray isArray
+    keys keys keys keys keys
+    values values values values values
+    entries entries entries entries entries
+    fill fill fill fill fill
+    find find find find find 
+    findIndex findIndex findIndex findIndex findIndex
+    Array.from Array.from Array.from Array.from
+    replace replace replace replace replace
+    indexOf indexOf indexOf indexOf indexOf
 
-function buildTree(data) {
-    let tree = [];
-    for (let i = 0; i < data.length; i++) {
-        const item = data[i];
-        if (item.pid === 0) {
-            tree.push(item);
-        } else {
-            const parent = data.find(p => p.id === item.pid);
-            if (parent) {
-                const children = parent.children || [];
-                children.push(item);
-                parent.children = children;
-            }
-        }
+*/
+
+/* 
+    字符串
+    concat concat concat concat concat
+    slice slice slice slice slice
+    substring substring substring substring substring
+    substr substr substr substr substr
+    indexOf indexOf indexOf indexOf indexOf indexOf
+    lastIndexOf lastIndexOf lastIndexOf lastIndexOf lastIndexOf
+    includes includes includes includes includes
+    trim trim trim trim trim
+    repeat repeat repeat repeat repeat
+    toUpperCase toUpperCase toUpperCase toUpperCase toUpperCase
+    toLowerCase toLowerCase toLowerCase toLowerCase toLowerCase
+    match match match match match
+    search search search search search
+    replace replace replace replace replace
+    split split split split split
+
+    concat concat concat concat concat
+    slice slice slice slice slice
+    substring substring substring substring substring
+    substr substr substr substr substr
+    trim trim trim trim trim 
+    repeat repeat repeat repeat repeat
+    replace replace replace replace replace
+
+*/
+
+const arr = [1, 2, 3];
+console.log(arr.findIndex(item => item > 1));
+console.log(arr)
+const arr2 = [
+    {
+        num: 1
+    },
+    {
+        num: 2
     }
-    return tree;
-}
-
-const tree = buildTree(data);
-console.log(JSON.stringify(tree, null, 2));
+]
+// console.log(Array.from(arr.keys()))
 
