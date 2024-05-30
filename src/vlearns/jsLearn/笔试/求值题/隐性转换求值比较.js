707e8cb7e975cf3538cc3033aac20b7a73d16f4a
homeScl <419863966@qq.com>
2024-05-30 23:02:13 +0800

    求值题整理

--- src/vlearns/jsLearn/笔试/求值题/隐性转换求值比较.js
+++ src/vlearns/jsLearn/笔试/求值题/隐性转换求值比较.js
@@ -1,5 +1,13 @@
+/* 
+原因：
+    1. 隐性转换
+    2. "9"的Ascall码是57，"10"的Ascall码是48
+*/
+
 var a = [9];
 var b = [10];
 console.log(a == 9);
 console.log(b == 10);
 console.log(a < b);
+
+console.log( '9' < '10' );
